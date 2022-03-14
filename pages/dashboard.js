68af58ac2bc473e37bf13c14b4be1581e6c437e3
PyPy <skfelix1@gmail.com>
2022-03-14 14:20:41 -0700

    mobile view fix

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,9 +8,9 @@ function dashboard() {
 
     return (
       <div className='absolute inset-0'>
-        <html className="h-screen bg-gray-50">
-          <body className="h-screen overflow-hidden">
-             <div className=" max-h-screen flex">
+        <html className="absolute inset-0 h-screen bg-gray-50">
+          <body className="absolute inset-0 h-screen overflow-hidden">
+             <div className="absolute inset-0 max-h-screen flex">
         {/* <html className="h-screen bg-gray-50">
           <body className="h-full overflow-hidden">
              <div className="h-full flex"> */}
