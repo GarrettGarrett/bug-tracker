eea82b4b5de16c91cb4dde6a1084ece8760af655
PyPy <skfelix1@gmail.com>
2022-03-17 04:28:11 -0700

    adds link source for github next image

--- components/RoleAssignment.js
+++ components/RoleAssignment.js
@@ -135,6 +135,7 @@ function RoleAssignment({session}) {
             
                 <div className='md:pt-8'>
                     {/* third column */}
+                   
                     {
                         data ? 
                     <button
@@ -152,6 +153,12 @@ function RoleAssignment({session}) {
                                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
                         }
+                        {/* { 
+                   party &&
+                    <div className='absolute w-40 -mt-5 pl-32 '>
+                        <PartyLottie />
+                    </div>} */}
+
                     
                     </button>
                     :
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 
 module.exports = {
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
